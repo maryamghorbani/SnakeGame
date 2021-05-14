488931a595998bb4fb9f6919572e74c00eb73133
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-15 00:22:59 +0430

    walking snake

--- js/script.js
+++ js/script.js
@@ -1,47 +1,80 @@
 const gameCanvas = document.getElementById("gameCanvas");
+
 const ctx = gameCanvas.getContext('2d');
 
-ctx.fillStyle = 'white'
-ctx.strokeStyle = 'black'
-ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
-ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height)
-
 let snake = [
-    { x: 150 , y : 150},
-    { x: 140 , y : 150},
-    { x: 130 , y : 150},
-    { x: 120 , y : 150},
-    { x: 110 , y : 150},
+    { x : 150 , y : 150 },
+    { x : 140 , y : 150 },
+    { x : 130 , y : 150 },
+    { x : 120 , y : 150 },
+    { x : 110 , y : 150 },
 ]
-
-snake.forEach(snakePart => {
-    ctx.fillStyle = 'lightgreen';
-    ctx.strokeStyle = 'black';
-
-    ctx.fillRect(snakePart.x , snakePart.y , 10 ,10);
-    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10)
-})
 
 let foodX;
 let foodY;
 
-let randomNumber = (max , min) => Math.round((Math.random() * (max-min) + min) /10) * 10
+
+function main() {
+    // game over
+    setTimeout(() => {
+        clearCanvas()
+        drawFood()
+        advanceSnake()
+        drawSnake()
+
+        main();
+    }, 100);
+}
+
+
+let clearCanvas = () => {
+    ctx.fillStyle = 'white'
+    ctx.strokeStyle = 'black'
+
+    ctx.fillRect(0 , 0 , gameCanvas.width , gameCanvas.height)
+    ctx.strokeRect(0,0 ,gameCanvas.width, gameCanvas.height)
+}
+
+let randomNumber = (max , min) => Math.round((Math.random() * (max - min) + min) / 10) * 10
 
 let createFood = () => {
-    foodX = randomNumber(0 , gameCanvas.width -10);
-    foodY = randomNumber(0 , gameCanvas.height -10);
-
+    foodX = randomNumber(0,gameCanvas.width - 10);
+    foodY = randomNumber(0,gameCanvas.height - 10);
     snake.forEach(snakePart => {
-        if ( snakePart.x == foodX && snakePart.y == foodY ) {
+        if(snakePart.x === foodX && snakePart.y === foodY) {
             createFood()
         }
     })
 }
 
+let advanceSnake = () => {
+    const head = { x : snake[0].x + 0 , y : snake[0].y - 10}
+
+    snake.unshift(head);
+
+    snake.pop()
+
+}
+
+let drawSnake = () => snake.forEach(drawSnakePart)
+let drawSnakePart = snakePart => {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeStyle = 'black'
+
+    ctx.fillRect(snakePart.x , snakePart.y , 10 , 10);
+    ctx.strokeRect(snakePart.x , snakePart.y , 10 , 10);
+}
+
+let drawFood = () => {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'darkred'
+
+    ctx.fillRect(foodX , foodY , 10 , 10)
+    ctx.strokeRect(foodX , foodY , 10 , 10)
+}
+
 createFood()
 
-ctx.fillStyle = 'red';
-ctx.strokeStyle = 'black';
+// let foodY
 
-ctx.fillRect(foodX , foodY, 10, 10)
-ctx.strokeRect(foodX , foodY, 10, 10)
+main()
